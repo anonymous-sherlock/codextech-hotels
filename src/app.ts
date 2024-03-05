cdf1a3ec2c553f3e4b4d333d34f24a1d3f777caa
Anonymous Sherlock <layalakash4@gmail.com>
2024-03-05 12:32:49 +0530

    fix: routes changes

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,6 @@ import { Pool } from '@neondatabase/serverless';
 import { drizzle } from 'drizzle-orm/neon-serverless';
 import { cors } from 'hono/cors';
 import * as schema from "./db/schema";
-import authhandlers from './routes/auth.route';
-
-
 app.use('/*', cors())
 app.use(async (ctx, next) => {
   const connection = new Pool({ connectionString: ctx.env.DATABASE_URL });
@@ -14,8 +11,6 @@ app.use(async (ctx, next) => {
 });
 
 // routers
-app.get("/api/auth", ...authhandlers)
-
-app.get("/", (c) => c.json(c.env))
+app.get("/", (c) => c.json({ message: "Heelo Garibo" }))
 
 export default app
